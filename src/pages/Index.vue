e8a2b759246999c35eeed26655d41e3943b25c99
RobKraft <rob@robkraft.org>
2020-07-09 10:56:09 -0500

    Try another quick way for DialogFlow

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -433,3 +433,10 @@ query {
   } 
 }
 </page-query>
+
+<script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1" defer></script>
+    <df-messenger intent="WELCOME"
+                  chat-icon="/images/Robo-Icon.png"
+                  chat-title="Dining-Out"
+                  agent-id="a16ae295-e755-4f0e-9fc0-d111798c5ca6"
+                  language-code="en"></df-messenger>
