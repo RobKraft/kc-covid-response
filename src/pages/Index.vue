aceac04af7f991261b5a9f87e3c0f588fe3d9aa6
RobKraft <rob@robkraft.org>
2020-07-16 13:26:54 -0500

    Fornatting testing loc map stuff

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -204,15 +204,18 @@
       </TwoColumnSection>
     </div>
     <OneColumnSection class="pt-16 pb-4 md:pt-32 md:pb-8">
-      <h2 class="px-4 text-3xl">Health Inequities</h2>
+      <h2 class="px-4 text-3xl">KC COVID TESTING LOCATIONS</h2>
       <div class="px-4 md:pr-16"><p>
-      This map shows locations where you may be able to get testing for COVID-19 in Kansas City. Please click on the location pin on the map to see details about who can get a test and how to start the process. Each site has different criteria and processes.  This map doesn't include temporary, pop-up testing sites. To see those, check our [Testing Events page](https://www.comebackkc.com/testing-events/). 
+      This map shows locations where you may be able to get testing for COVID-19 in Kansas City. Please click on the location pin on the map to see details about who can get a test and how to start the process. Each site has different criteria and processes.  This map doesn't include temporary, pop-up testing sites. To see those, check our <a href="https://www.comebackkc.com/testing-events/">[Testing Events page].</a>
 
-**IMPORTANT - call ahead to discuss your situation *BEFORE* going to get tested.** 
+<div><b>IMPORTANT - call ahead to discuss your situation *BEFORE* going to get tested.</b></div>
 
 <iframe height="600px" width="100%" style="border:none;" src="https://view-awesome-table.com/-M7IzhZjxvfs3UpYvW0Z/view"></iframe>
 
-**NOTE** - This information changes rapidly, and we are making a concerted effort to keep it up-to-date and complete. We cannot guarantee that this information is current, so you ***must*** call the testing site to verify details and discuss your situation before you visit. If you know of a testing location that isn't shown here, please use [this form](https://forms.gle/fTWftBj6Wen3G4yRA) to let us know about it.
+<b>NOTE</b> - This information changes rapidly, and we are making a concerted effort to keep it up-to-date
+ and complete. We cannot guarantee that this information is current, so you <b>must</b> call the testing site to 
+ verify details and discuss your situation before you visit. If you know of a testing location that isn't shown here,
+  please use <a href="https://forms.gle/fTWftBj6Wen3G4yRA">[this form]</a> to let us know about it.
 
       </p></div>
     </OneColumnSection>
